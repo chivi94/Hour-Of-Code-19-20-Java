439787cc3d69703f9c282ca394651cbfc2169761
Ivan <ivan.gonzalez.rincon>
2019-10-24 19:45:21 +0200

    Se ha creado el ejemplo de los bucles

--- EjemploBucles.java
+++ EjemploBucles.java
@@ -7,7 +7,9 @@ public class EjemploBucles {
          * siguientes: -> do-while: Se ejecuta al menos una vez, ya que la condicion se
          * evalua al final del bloque. -> while: Puede no llegar a ejecutarse si se
          * cumple la condicion de finalizacion del bucle al inicio del mismo. -> for: Se
-         * ejecuta un numero determinado de veces, es decir, esta controlado por indice.
+         * ejecuta un numero determinado de veces, es decir, esta controlado por indice,
+         * y conceptualmente, es igual que un bucle while.
+         * 
          * Los anteriores pueden controlarse por indice o por bandera(ahora veremos lo
          * que es esto). Al fin y al cabo, son igualmente condiciones booleanas.
          */
